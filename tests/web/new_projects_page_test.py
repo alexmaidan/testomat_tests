01c26e71094786395d1ec5a024bd4eccd87ad7fc
Oleksii Maidan <alex.maidan@gmail.com>
2026-01-27 04:28:52 +0200

    MNFCTR-3 Implemented Chain of Invocation Pattern

--- tests/web/new_projects_page_test.py
+++ tests/web/new_projects_page_test.py
@@ -1,24 +1,18 @@
 from faker import Faker
-
 from playwright.sync_api import Page
-
 from src.web.pages.NewProjectsPage import NewProjectsPage
 from src.web.pages.ProjectPage import ProjectPage
 
 
-def test_new_project_page_elements(page: Page,login):
-    new_projects = NewProjectsPage(page)
-    new_projects.open()
-    new_projects.is_loaded()
-
-def test_new_project_creation(page: Page,login):
-    new_projects = NewProjectsPage(page)
-    new_projects.open()
-    new_projects.is_loaded()
-
+def test_new_project_creation(page: Page, login):
     target_project_name = Faker().company()
-    new_projects.fill_project_title(target_project_name)
-    new_projects.click_create()
+    (NewProjectsPage(page)
+     .open()
+     .is_loaded()
+     .fill_project_title(target_project_name)
+     .click_create())
 
-    ProjectPage(page).is_loaded()
-
+    (ProjectPage(page)
+     .is_loaded()
+     .empty_project_name_is(target_project_name)
+     .close_read_me())
